784441397a85fe55ce0a1f3851589f1fdc953461
sancher6 <sancher6@tcnj.edu>
2017-09-26 13:36:05 -0400

    Add files via upload

---
+++ Subscriber.rb
@@ -0,0 +1,13 @@
+=begin
+Richard Bustamante
+CSC 415
+Assignment1
+=end
+require './Set.rb'
+
+class Subscriber
+    def initialize(acct, name)
+        @acct = acct    
+        @name = name
+    end
+end
